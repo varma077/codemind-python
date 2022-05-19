0fc8e4fa6afb97d31655ebe2714241d8cafbb4ab
varma077 <97425766+varma077@users.noreply.github.com>
2022-05-19 21:17:31 +0530

    t=int(input()) arr=list(map(int,input().split())) a,b=map(int,input().split()) s=0 for i in arr:     if(i>=a and i<=b):         print(i,end=" ")         s+=1 if(s==0):     print(-1)

---
+++ elements_in_between_A_and_B.py
@@ -0,0 +1,10 @@
+t=int(input())
+arr=list(map(int,input().split()))
+a,b=map(int,input().split())
+s=0
+for i in arr:
+    if(i>=a and i<=b):
+        print(i,end=" ")
+        s+=1
+if(s==0):
+    print(-1)
