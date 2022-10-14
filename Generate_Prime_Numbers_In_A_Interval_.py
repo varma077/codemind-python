57b33dbfa8f076c7eb7d12c675134a941ea68fd0
varma077 <97425766+varma077@users.noreply.github.com>
2022-10-14 18:24:30 +0530

    a=int(input()) b=int(input()) c=0 i=a+1 flag=0 while(i<b):     flag=0     j=2     while(j<=i/2):         if(i%j==0):             flag=1             break         j=j+1     if(flag==0):         print(i)     i=i+1

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=int(input())
+c=0
+i=a+1
+flag=0
+while(i<b):
+    flag=0
+    j=2
+    while(j<=i/2):
+        if(i%j==0):
+            flag=1
+            break
+        j=j+1
+    if(flag==0):
+        print(i)
+    i=i+1
